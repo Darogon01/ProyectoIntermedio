27a415bf5cd86a24f84c24825bc70817612ccfc1
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-08 20:51:40 +0200

    usrMovies structure + usrMovies.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,6 @@ const pages = require('../controllers/routes')
 router.get('/', pages.home)
 router.get('/signup', pages.signup)
 router.get('/dashboard', pages.dashboard)
+router.get('/movies', pages.movies)
 
 module.exports = router
