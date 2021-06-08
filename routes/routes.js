cc53196f80271c54bed14699f6ad9501a7930753
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-08 08:56:13 +0200

    add reset.css + fix signup.pug

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const pages = require('../controllers/routes')
 
 router.get('/', pages.home)
+router.get('/signup', pages.signup)
 
 module.exports = router
