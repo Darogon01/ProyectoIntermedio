3a98b1531b217c9903c670182624eae9c52f29da
DvDRo5 <dromang5@gmail.com>
2021-06-09 17:26:22 +0200

    add Styls film

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const pages = require('../controllers/routes')
 
 router.get('/', pages.home)
 router.get('/signup', pages.signup)
-router.get('/search/:title', pages.search)
+router.get('/search/:title', pages.film)
 router.get('/dashboard', pages.dashboard)
 router.get('/movies', pages.movies)
 
