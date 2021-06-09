d3115bb78764f9c7a308396473b52e84d5400cdf
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-09 19:33:27 +0200

    add editMovie structure

--- routes/routes.js
+++ routes/routes.js
@@ -7,5 +7,7 @@ router.get('/dashboard', pages.dashboard)
 router.get('/movies', pages.movies)
 router.get('/createMovie', pages.createMovie)
 router.post('/createMovie', pages.createMoviePost)
+router.get('/editMovie/:id', pages.editMovie)
+router.post('/editMovie', pages.editMoviePut) //CONSULTAR POR QUE NO FUNCIONA CON PUT
 
 module.exports = router
