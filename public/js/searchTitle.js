7265906198d7a81c273f41bea6b327a6f3874e8a
Darogon01 <78644182+Darogon01@users.noreply.github.com>
2021-06-12 17:23:07 +0200

    add estructure scrapping

--- public/js/searchTitle.js
+++ public/js/searchTitle.js
@@ -1,1 +1,16 @@
-console.log('searchTitle')
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+/* const path = require('path');
+const opinionss = require('./.json'); */
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  /* for (const opinions of opinionss) {
+    const scriptPath = path.join(__dirname, 'scripts', opinions.scriptName); */
+    await require(scriptPath)(page, opinions);
+    console.log('Scraping done for', opinions.name);
+  /* } */
+  await page.close();
+  await browser.close();
+})();
