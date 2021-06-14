dc087182f658b10ca3fd9526535a4e211682652b
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-14 20:28:42 +0200

    db sql created + model/Users.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 const express = require('express')
 
-const router = require("./routes/routes")
 require('./utils/db')
+const router = require("./routes/routes")
 
 const app = express()
 
---
+++ models/Users.js
@@ -0,0 +1,55 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'proyect_inter',
+    connectionLimit: 5
+});
+
+const User = {
+    createUser: async(user_data) => {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const sql_query = 'INSERT INTO users (email,password,token) value (?,?,?)'
+            const res = await conn.query(sql_query, user_data);
+            console.log(res);
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.end();
+        }
+    },
+    getAllUsers: async() => {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            const sql_query = 'SELECT * FROM users'
+            const res = await conn.query(sql_query);
+            console.log(res);
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.end();
+        }
+    },
+    getUser: async(id) => {
+            let conn;
+            try {
+                conn = await pool.getConnection();
+                const sql_query = 'SELECT * FROM users WHERE id = ?'
+                const res = await conn.query(sql_query, [id]);
+                console.log(res);
+            } catch (err) {
+                throw err;
+            } finally {
+                if (conn) return conn.end();
+            }
+        }
+        // User.getUser(2)
+        // User.getAllUsers()
+        // User.createUser(["juanma@mail.com", "123456", "fdg32afdsg321354g6a54dfg"])
+}
+
+module.exports = User
