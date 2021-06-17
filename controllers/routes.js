ddb491761837c90f05d560aa26c54002863d1688
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-17 17:31:40 +0200

    search for favorite movies in db & api

--- controllers/routes.js
+++ controllers/routes.js
@@ -21,17 +21,15 @@ const routes = {
         let titleMayus = capitalizarPrimeraLetra(title);
 
         function capitalizarPrimeraLetra(str) {
-            console.log(str)
             return str.charAt(0).toUpperCase() + str.slice(1);
         }
 
         let data = await films.getPelicula(
             `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`
         );
-        console.log(data, "data");
 
         async function opinionsSensa() {
-            const browser = await puppeteer.launch({ headless: false });
+            const browser = await puppeteer.launch( /*{ headless: false }*/ );
             const page = await browser.newPage();
             await page.goto(`https://www.sensacine.com`);
             await page.waitForSelector("#didomi-notice-agree-button");
@@ -48,28 +46,22 @@ const routes = {
                 `#search-engine > div > div > div.autocomplete-results > div > img[alt=${titleMayus}]`
             );
             await page.waitForSelector(".content-txt.review-card-content");
-            /* const opinions = document.querySelectorAll(
-                  ".content-txt.review-card-content"); */
             const coments = await page.evaluate(() => {
-                console.log("estamos dentro");
                 const opinions = document.querySelectorAll(
                     ".content-txt.review-card-content"
                 );
-                /* console.log(opinions); */
                 const dataComentSensa = [];
 
                 opinions.forEach((comentarios) => {
-                    console.log(comentarios);
                     dataComentSensa.push(comentarios.innerText);
                 });
 
                 return dataComentSensa;
             });
-            console.log(coments);
             return coments;
         }
         async function opinionsAfinity() {
-            const browser = await puppeteer.launch({ headless: false });
+            const browser = await puppeteer.launch( /*{ headless: false }*/ );
             const page = await browser.newPage();
             await page.goto(`https://www.filmaffinity.com/es/main.html`);
             await page.waitForSelector("#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-47sehv");
@@ -82,21 +74,17 @@ const routes = {
             await page.click("#title-result > div > div:nth-child(2) > div.fa-shadow-nb.item-search > div > div.mc-poster > a > img");
             await page.waitForSelector('div[itemprop=reviewBody]');
             const coments = await page.evaluate(() => {
-                console.log("estamos dentro");
                 const opinionsAfinity = document.querySelectorAll(
                     'div[itemprop=reviewBody]'
                 );
-                console.log(opinionsAfinity);
                 const dataComentAfinity = [];
 
                 opinionsAfinity.forEach((comentarios) => {
-                    console.log(comentarios);
                     dataComentAfinity.push(comentarios.innerText);
                 });
 
                 return dataComentAfinity;
             });
-            console.log(coments);
             return coments;
         }
 
@@ -108,16 +96,51 @@ const routes = {
     movies: async(req, res) => {
         // let arrFavoritas = ["tt1216475", "tt4029846", "tt10222892", "tt0401383"]
         let favs = await User.getUserFavorites("juanma@mail.co") //FALTA LA OBTENCION DEL EMAIL DE USUARIO
-        let arrFavoritas = favs.map(fav => Object.values(fav))
-        let getFilms = async() => {
-            let pelis = arrFavoritas.map(async(filmID) => {
-                let data = await films.getPelicula(`http://www.omdbapi.com/?i=${filmID}&apikey=${apiKey}`)
-                return data
-            })
-            return Promise.all(pelis)
+        let arrFavoritasApi = []
+        let arrFavoritasDB = []
+        let getFilmsApi
+        let getFilmsDB
+        favs.forEach(fav => {
+            let id = Object.values(fav)
+            const regexIdApi = /^tt/
+            regexIdApi.test(Object.values(fav)) ? arrFavoritasApi.push(id) : arrFavoritasDB.push(id)
+        })
+        if (arrFavoritasApi) {
+            getFilmsApi = async() => {
+                let pelis = arrFavoritasApi.map(async(filmID) => {
+                    let data = await films.getPelicula(`http://www.omdbapi.com/?i=${filmID}&apikey=${apiKey}`)
+                    return data
+                })
+                return Promise.all(pelis)
+            }
         }
-        let data = await getFilms()
-        res.status(200).render('movies', { data })
+        if (arrFavoritasDB) {
+            getFilmsDB = async() => {
+                try {
+                    let pelis = arrFavoritasDB.map(async(filmID) => {
+                        let data = await Film.find({ "filmId": filmID })
+                        return data[0]
+                    })
+                    return Promise.all(pelis)
+                } catch (err) {
+                    res.end()
+                }
+            }
+        }
+        let favorites = await getFilmsApi()
+        let filmsDB = await getFilmsDB()
+        filmsDB.forEach(film => {
+            favorites.push({
+                imdbID: film.filmId,
+                Poster: film.urlImage,
+                Title: film.title,
+                Year: film.year,
+                Director: film.director,
+                Genre: film.genre,
+                Runtime: film.runtime,
+            })
+        })
+        res.status(200).render('movies', { favorites })
     },
     search: async(req, res) => {
         // SUSTITUIR POR LA RESPUESTA DE LA BBDD
@@ -191,10 +214,8 @@ const routes = {
             let favorite = await User.searchFavorite(email, api_id_film)
             let id_film = favorite[0] != undefined ? favorite[0].id_film : false
             if (id_film) {
-                console.log(`existe`)
                 await User.removeFavorite(email, api_id_film)
             } else {
-                console.log(`no existe`)
                 await User.addFavorite(email, api_id_film)
             }
             return res.status(201).redirect(`/movies`)
